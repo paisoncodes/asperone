82b834562b82b537c106d679c61aa29acc62f521
Komolafe Olatunji <68442675+paisoncodes@users.noreply.github.com>
2022-12-16 11:40:51 +0100

    Update views.py

--- fatgbems/views.py
+++ fatgbems/views.py
@@ -71,7 +71,57 @@ class PaymentNotification(APIView):
 
 class PaymentReversal(APIView):
     def post(self, request):
-        data = request.data
+        new_data = request.data
+        new_data = request.data
+        try:
+            headers["Authorization"] = request.headers["Authorization"]
+        except Exception:
+            return Response(
+                {
+                    "message": "Authorization should not be empty"
+                },
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+        
+        try:
+            new_data["TerminalID"] = request.headers["TerminalID"]
+        except Exception:
+            return Response(
+                {
+                    "message": "TerminalID should not be empty"
+                },
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        data = {
+            "data": new_data
+        }
+
+        response = requests.post(PAYMENT_REVERSAL_URL, data=json.dumps(data), headers=headers)
+        new_response = response.json()
+        if response.status_code == 401:
+            return Response(new_response["message"], status=status.HTTP_401_UNAUTHORIZED)
+        elif response.status_code == 400:
+            return Response(new_response, status=status.HTTP_400_BAD_REQUEST)
+        elif response.status_code == 404:
+            return Response(new_response, status=status.HTTP_403_FORBIDDEN)
+        elif response.status_code == 200:
+            return Response(new_response, status=status.HTTP_200_OK)
+        elif response.status_code == 403:
+            return Response(
+                {
+                    "message": "Invalid Authorization"
+                },
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+        else:
+            return Response(
+                {
+                    "message": new_response,
+                    "code": response.status_code
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
 
         
     
